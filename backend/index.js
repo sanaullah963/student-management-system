7d24f51e44e2554ae87638aa18a3f526a344ceba
sanaullah963 <allhera963@gmail.com>
2023-11-23 16:36:59 +0600

    get MongoDb data

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,26 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const mongoDB = require("mongodb");
 const cors = require("cors");
 const student = require("./module/student");
 const teacher = require("./module/teacher");
-const notice = require('./module/notice')
+const notice = require("./module/notice");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 console.log("................................");
 
-// running server
-app.listen(8000, () => {
-  console.log("server is running in port " + process.env.PORT);
+// root route
+app.get("/", (req, res) => {
+  res.send("hello");
 });
+
+// running server
+app.listen(8000, () =>
+  console.log("server is running in port " + process.env.PORT)
+);
 
 // connect mongoDB
 const connect = async () => {
@@ -23,17 +29,39 @@ const connect = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("mongoDB is connected");
   } catch (error) {
-    console.log("mongoDB is not connect, error : ", error);
+    console.log("mongoDB connetn error : ", error);
   }
 };
 connect();
 
-// create a get route
-app.get("/", (req, res) => {
-  res.send("hello");
+// student get route
+app.get("/students", async (req, res) => {
+  try {
+    const students = await student.find();
+    res.send(students);
+  } catch (error) {
+    console.log("get mongoDB data to student route error : ", error);
+  }
+});
+//teachers get route
+app.get("/teachers", async (req, res) => {
+  try {
+    const teachers = await teacher.find();
+    res.send(teachers);
+  } catch (error) {
+    console.log("get mongoDB data to teachers route error : ", error);
+  }
+});
+// notices get route
+app.get("/notices", async (req, res) => {
+  try {
+    const notices = await notice.find();
+    res.send(notices);
+  } catch (error) {
+    console.log("get mongoDB data to notices route error : ", error);
+  }
 });
 
-// post route
 // student post route
 app.post("/student", async (req, res) => {
   // destracture std data data
@@ -68,7 +96,6 @@ app.post("/student", async (req, res) => {
 
   return res.send("New Suedent added successfully");
 });
-
 // teacher post route
 app.post("/teacher", async (req, res) => {
   // destracture data
@@ -87,16 +114,13 @@ app.post("/teacher", async (req, res) => {
   newTeacher.save();
   return res.send("Teacher Added Successfully");
 });
-
-// notice post route 
-app.post('/notice',async(req,res)=>{
-  const {description,headline} = await req.body
+// notice post route
+app.post("/notice", async (req, res) => {
+  const { description, headline } = await req.body;
   const newNotic = new notice({
     headline,
     description,
-  })
-  newNotic.save()
-  return(
-    res.send('notice added successfull')
-  )
-})
+  });
+  newNotic.save();
+  return res.send("notice added successfull");
+});
